24f47211c513add8f6b93709fb3504876ed19dcf
Nguyen Xuan Binh <70772812+SpringNuance@users.noreply.github.com>
2022-01-10 21:24:33 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 import requests, jwt
 import os
+import uuid
 from dotenv import load_dotenv
-from pprint import pprint
 from urllib.parse import urlparse, parse_qs
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 def API():
     load_dotenv()
@@ -27,14 +27,16 @@ def API():
     # The base URL for banking API
     url = "https://api.tilisy.com"
 
-    # The authorization headers, sent in every request to any api addresses
+    # The authorization headers, sent in every request to api addresses
     authHeaders = { "Authorization" : f"Bearer {token}"}
     
     # application information
     response = requests.get(f"{url}/application", headers=authHeaders)
     appDetails = response.json()
     print("The application details")
-    pprint(appDetails)
+    for detail in appDetails:
+        print(detail + ": ", end="")
+        print(appDetails[detail])
 
     # Getting the bank details 
     response = requests.get(f"{url}/aspsps", headers=authHeaders)
@@ -54,34 +56,105 @@ def API():
     # JSON body sent in POST request to /auth
     body = {
         "access": {
-            "valid_until": "2022-02-01T00:00:00Z" # Change date to the future
+            "valid_until": (datetime.now(timezone.utc) + timedelta(days=30)).isoformat()
+            # Change date to the future, possibly one month ahead of your current time
         },
         "aspsp": {
             "country": country,
             "name": bank
         }, # Account Servicing Payment Service Providers
-        "state": "3a57e2d3-2e0c-4336-af9b-7fa94f0606a3",
+        "state": str(uuid.uuid4()),
         "redirect_url": appDetails["redirect_urls"][0],
     }
 
     # Authentication Post request
     response = requests.post(f"{url}/auth", json=body, headers=authHeaders)
-    pprint(response.json())
     authenticationUrl = response.json()["url"]
     print(f'To authenticate your bank account, paste into your browser: {authenticationUrl}')
  
     
-    # Reading auth code and creating user session
-    redirected_url = input("Please paste into the console the URL that you were redirected to: ")
-    # The code 
-    print(parse_qs(urlparse(redirected_url).query))
-    code = parse_qs(urlparse(redirected_url).query)["code"][0]
-    print(code)
-    response = requests.post(f"{url}/sessions", json= {"code": code}, headers=authHeaders)
-    print("Information of the session")
-    session = response.content
-    pprint(session)
+    # User session can be created from captured code parameter in the redirected link from authorization request
+    redirectUrl = input("After authentication, please paste into the console the URL that you were redirected to: ")
+    # The code in the redirected link 
+    capturedCode = parse_qs(urlparse(redirectUrl).query)["code"][0]
+    print("\nThe code in the authenticate redirected link is\n" + capturedCode)
+    response = requests.post(f"{url}/sessions", json= {"code": capturedCode}, headers=authHeaders)
+    
+    # Session details are obtained from the response
+    session = response.json()
 
+    # Find the retrieved bank account from IBAN number
+    
+    IBAN = input("\nPlease input your IBAN number: ")
+    retrievedAccount = list(filter(lambda account : account["account_id"]["iban"] == IBAN, session["accounts"]))[0]
+
+    # Account details
+    print("\n-----Your account information-----\n")
+    for detail in retrievedAccount:
+        print(detail + ": ", end="")
+        print(retrievedAccount[detail])
+
+    # Retrieving account balances
+    uid = retrievedAccount["uid"]
+    response = requests.get(f"{url}/accounts/{uid}/balances", headers=authHeaders)
+    print("\n-----Your bank balances-----\n")
+    balances = response.json()
+    for balance in balances['balances']:
+        for detail in balance:
+            if (detail == "balance_amount"):
+                print(detail + ": ", end="")
+                print(balance[detail]["amount"] + " " + balance[detail]["amount"])
+            else:    
+                print(detail + ": ", end="")
+                print(balance[detail])
+        print('\n')
+    
+    # Transaction details within the last 30 days
+    input("Press Enter to continue to see your transactions in the last 30 days...")
+    
+    chosenKeys = ["credit_debit_indicator","reference_number","transaction_amount","transaction_date"]
+    print("\n-----Transactions in the last 30 days-----\n:")
+    numTrans, max, credit, debit = 0
+    query = {
+        "date_from": (datetime.now(timezone.utc) - timedelta(days=30)).date().isoformat(),
+    }
+    continuationKey = None
+    while True:
+        # In each get request, continuation key is checked with the date_from property
+        if continuationKey:
+            query["continuation_key"] = continuationKey
+        response = requests.get(f"{url}/accounts/{uid}/transactions", params=query, headers=authHeaders)
+        # data is an object that has 2 properties continuation key and transactions object.   
+        data = response.json()
+        transactions = data["transactions"]
+        for transaction in transactions:
+            for detail in transaction:
+                if detail in chosenKeys:
+                    print(detail + ": ", end="")
+                    print(transaction[detail])
+            amount = float(transaction['transaction_amount']['amount']) # Transaction amount
+            if (transaction["credit_debit_indicator"] == 'CRDT'):
+                credit += amount # Find total credit transactions
+            else:
+                debit += amount # Find total dedit transactions
+            if (amount > max):
+                max = amount # Find maximum transaction
+            print("")
+            numTrans += 1 # Find total transaction
+        continuationKey = data.get("continuation_key")
+        # If surpassing the date_from, the continuation_key would become undefined/None to break the loop
+        if not continuationKey:
+            break
+
+    # Short summary of the transactions for the last 30 days
+    print("\n-----Transaction Summary in 30 Days-----\n")
+    print("Number of transactions: " + str(numTrans))
+    print("Transaction with maximum value: " + str(max) + " euros")
+    print("Total credit transactions: " + str(credit) + " euros")
+    print("Total debit transactions: " + str(debit) + " euros")
+
+    # Enable Banking only operates in Europe, so it is assumed that the currency is Euro
+    
 
 API() # run the application
 
